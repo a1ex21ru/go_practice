d7662afb0de44088e884331b04efe5e51fb096f7
a1ex21ru <r1nin@mail.ru>
2025-08-10 13:38:33 +0300

    fff

---
+++ 1/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	//	. "golang-ninja/basic/shape"
+)
+
+const winePrice = 100
+
+func main() {
+	// user := User{"Vasya", 23, "male", 78, 180}
+	// users := []User{user}
+	// users = append(users, User{"fot", 55, "male", 44, 100})
+	//user.printff()
+
+	change, err := buy_wine(177, 101)
+	if err != nil {
+		fmt.Println("failed: ", err)
+	} else {
+		fmt.Println("your change: ", change)
+	}
+
+}
+
+func buy_wine(age, money int) (int, error) {
+	if age >= 18 && money >= winePrice {
+		return money - winePrice, nil
+	} else if age < 18 {
+		return money, errors.New("sorry, you are little")
+	} else {
+		return money, errors.New("don't enough money")
+	}
+}
---
+++ practice_6_10/generics.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+
+	. "golang.org/x/exp/constraints"
+)
+
+func main() {
+	list := []int{1, 2, 3, 4}
+	listAge := []int{15, 24, 75, 19}
+	//fmt.Println(IsContains(4, list))
+	//fmt.Println(IsContains(44, list))
+
+	list2 := []string{"apple", "banana", "malone", "orange"}
+	fmt.Println(IsContains("apple", list2))
+
+	//sum := func(x, y int) int { return x + y }
+	multy := func(x, y int) int { return x * y }
+
+	res := Reduce(list, multy, 1)
+	fmt.Println(res)
+
+	x2 := func(x int) int { return x * 2 }
+	fmt.Println(Map(list, x2))
+
+	filterFunc := func(x int) bool {
+		if x < 18 {
+			return false
+		}
+		return true
+	}
+	fmt.Println(Filter(listAge, filterFunc))
+}
+
+func IsContains[T comparable](n T, list []T) bool {
+	for _, v := range list {
+		if v == n {
+			return true
+		}
+	}
+	return false
+}
+
+func Max[T Ordered](x, y T) T {
+
+	if x > y {
+		return x
+	}
+	return y
+}
+
+type Number interface {
+	Integer | Float
+}
+
+func Sum[T Number](list []T) T {
+	var sum T
+	for _, v := range list {
+		sum += v
+	}
+	return sum
+}
+
+func Reduce[T any](list []T, accumulator func(T, T) T, init T) T {
+	for _, v := range list {
+		init = accumulator(init, v)
+	}
+
+	return init
+}
+
+func Filter[T any](list []T, f func(T) bool) []T {
+	var result []T
+	for _, v := range list {
+		if f(v) {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+func Map[T any](list []T, f func(T) T) []T {
+	var result []T
+	for _, v := range list {
+		result = append(result, f(v))
+	}
+	return result
+}
